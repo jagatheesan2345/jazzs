09735ab3a62c4331e86301b242368f8e54d25729
jagatheesan2345 <theesun6195@gmail.com>
2023-03-08 10:36:27 +0530

    jazz

--- src/org/github/prakash.java
+++ src/org/github/prakash.java
@@ -2,6 +2,12 @@ package org.github;
 
 public class prakash {
 
+	
+	public void kavi() 
+	{
+		System.out.println("ramkumar");
+
+	}
 	public static void main(String[] args)
 	{
 	System.out.println("hai praksh");
